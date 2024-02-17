6512be1414081630558e25dcd4c1079ad623c294
Collin Fiske <cfiske09@gmail.com>
2024-02-17 13:52:32 -0800

    fixed jinja

--- app.py
+++ app.py
@@ -26,15 +26,11 @@ allClubs = ["ACM (Association for Computing Machinery)",
 "Barkada of SCU",
 "AI"
 ]
-
+allClubs = sorted(allClubs)
 
 @app.route('/')
 def index():
-    # Read the content of clubs.html
-    #file_path=r"C:\Users\franc\OneDrive\Documentos\GitHub\H4H2024withfranco\templates\clubs.html"
-    
-    #with open(file_path, 'r') as file:
-        #clubs_content = file.read()
+   
 
     # Pass the content to the template for rendering
     return render_template('index.html', allClubs=allClubs)
@@ -42,8 +38,3 @@ def index():
 if __name__ == '__main__':
     app.run(debug=True)
 
-    # Render the template with data
-    #return render_template('index.html', name=name, age=age, interests=interests)
-
-#if __name__ == '__main__':
-    #app.run(debug=True)
