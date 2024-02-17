855c107bbe67600bc93f31f27ceba84c7b213c93
Franco Garcia <francoe_garcia@outlook.com>
2024-02-17 13:27:32 -0800

    menu

--- app.py
+++ app.py
@@ -4,13 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Sample data
-    name = "John"
-    age = 30
-    interests = ["Python", "Flask", "Jinja"]
+    # Read the content of clubs.html
+    file_path=r"C:\Users\franc\OneDrive\Documentos\GitHub\H4H2024withfranco\templates\clubs.html"
+    
+    with open(file_path, 'r') as file:
+        clubs_content = file.read()
 
-    # Render the template with data
-    return render_template('index.html', name=name, age=age, interests=interests)
+    # Pass the content to the template for rendering
+    return render_template('index.html', clubs_content=clubs_content)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    # Render the template with data
+    #return render_template('index.html', name=name, age=age, interests=interests)
+
+#if __name__ == '__main__':
+    #app.run(debug=True)
